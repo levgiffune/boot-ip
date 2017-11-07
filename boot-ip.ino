8574fc3bbde35f71f8b9945bbfbb8af8d7cc349a
Lev Giffune <levgiffune@gmail.com>
2017-11-07 12:12:49 -0500

    added comments to boot-ip.ino and added delays to loading animation

--- boot-ip.ino
+++ boot-ip.ino
@@ -1,16 +1,22 @@
+//import code to interface with the LCD
 #include <LiquidCrystal.h>
 
+//initialize LiquidCrustal with pin numbers
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
+  //start serial communications with computer
   Serial.begin(9600);
+  //tell LiquidCrystal the size of the LCD
   lcd.begin(16, 2);
-  lcd.print("Wiating for pi");
+  //print loading message
+  lcd.print("Waiting for pi");
   lcd.setCursor(0, 1);
   lcd.print("to boot.");
 }
 
 void loop() {
+  //if the computer recoginized us, print the ip
   if (Serial.available() > 0) {
     lcd.clear();
     lcd.setCursor(0, 0);
@@ -18,40 +24,48 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print(Serial.readString());
   }
+  //otherwise, display loading animation
   else {
     load();
   }
 }
 
-
+//loading animation
 void load() {
   lcd.clear();
   lcd.print("Wiating for pi");
   lcd.setCursor(0, 1);
   lcd.print("to boot");
+  sleep(1);
   lcd.clear();
   lcd.setCursor(0, 1);
   lcd.print("to boot.");
+  sleep(1);
   lcd.clear();
   lcd.print("Wiating for pi");
   lcd.setCursor(0, 1);
   lcd.print("to boot..");
+  sleep(1);
   lcd.clear();
   lcd.print("Wiating for pi");
   lcd.setCursor(0, 1);
   lcd.print("to boot...");
+  sleep(1);
   lcd.clear();
   lcd.print("Wiating for pi");
   lcd.setCursor(0, 1);
   lcd.print("to boot ..");
+  sleep(1);
   lcd.clear();
   lcd.print("Wiating for pi");
   lcd.setCursor(0, 1);
   lcd.print("to boot  .");
+  sleep(1);
   lcd.clear();
   lcd.print("Wiating for pi");
   lcd.setCursor(0, 1);
   lcd.print("to boot   ");
+  sleep(1);
 }
 
 
