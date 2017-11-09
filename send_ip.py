354a7a97f510d216cb079282cf56de79944700a3
Lev Giffune <levizgr8@Tracys-iMac.local>
2017-11-08 19:35:31 -0500

    added delay to python

--- send_ip.py
+++ send_ip.py
@@ -5,8 +5,8 @@ ip = os.system("curl ipinfo.io/ip")
 
 
 print ip
-arduino = serial.Serial('/dev/cu.usbmodem1411', 9600, timeout=.1)
-
+arduino = serial.Serial('/dev/cu.usbmodem1411', 9600, timeout=1)
+time.sleep(5)
 
 while 1:
     arduino.write(ip);
